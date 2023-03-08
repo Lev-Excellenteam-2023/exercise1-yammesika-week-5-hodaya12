4d9de29640d499b30b66aaaff0b0692d8975244b
hodaya12 <hodayaroz1@gmail.com>
2023-03-08 15:46:28 +0200

    update logo reading

--- 5.3.logo_reading.py
+++ 5.3.logo_reading.py
@@ -1,22 +1,24 @@
 logo_path="logo.jpg"
 
-def get_file(filename):
-    with open(filename, "rb") as f:
+def get_file(filename,buffer_size=2**10*8):
+    with open(filename,"rb") as f:
         x=''
-        while chunk := f.read():
+        while chunk := f.read(buffer_size):
             i = 0
             while i<len(chunk):
-                if(str(chunk)[i]>='a' and str(chunk)[i]<='z'):
-                    x=x+str(chunk)[i]
-                elif str(chunk)[i]=='!':
-                    x = x + str(chunk)[i]
-                    if(len(x))>=5:
-                        yield x
-                        x=''
-                else:x=''
+                try:
+                    if(chunk.decode(encoding="utf8", errors='ignore')[i]>='a' and chunk.decode(encoding="utf8", errors='ignore')[i]<='z'):
+                        x=x+chunk.decode(encoding="utf8", errors='ignore')[i]
+                    elif chunk.decode(encoding="utf8", errors='ignore')[i]=='!':
+                        x = x + chunk.decode(encoding="utf8", errors='ignore')[i]
+                        if(len(x))>=5:
+                            yield x
+                            x=''
+                    else:x=''
+                except:x=''
                 i=i+1
 
 
 
 for i in get_file(logo_path):
-    print("message is:  "+i)
+    print("message is:  "+i)
